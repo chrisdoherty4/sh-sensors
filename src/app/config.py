4462d1d449d201933129b7b46cb21806df55a48b
Chris Doherty <chris.doherty4@gmail.com>
2017-02-01 16:55:03 +0000

    Added dev mode configuration

--- src/app/config.py
+++ src/app/config.py
@@ -8,27 +8,56 @@ Created on 2 Jan 2017
 
 # Real values are dependent on the environment and desired sensitivity
 
+# Set development mode values
+DEV_MODE = True
 
-# Occupied state configuration
-occupied_config = {
-    # Time to wait for no detections thereby assuming the room is now unoccupied
-    # Seconds
-    "timeout": 10
-    }
-
-# Unoccupied state configuration
-unoccupied_config = {
-    # The time between consecutive detections in seconds that contribute to the 
-    # 'threshold' argument. We look for <= the interval between detections.
-    "max_detection_interval": 5, 
+if not DEV_MODE:
+    # Occupied state configuration
+    occupied_config = {
+        # Time to wait for no detections thereby assuming the room is now unoccupied
+        # Seconds
+        "timeout": 60*3
+        }
     
-    # The number of consecutive detections with the specified interval required 
-    # before we assume the room is occupied
-    "required_consecutive_detections": 1
-    }
+    # Unoccupied state configuration
+    unoccupied_config = {
+        # The time between consecutive detections in seconds that contribute to the 
+        # 'threshold' argument. We look for <= the interval between detections.
+        "max_detection_interval": 30, 
+        
+        # The number of consecutive detections with the specified interval required 
+        # before we assume the room is occupied
+        "required_consecutive_detections": 3
+        }
+    
+    # Represenst the hub room configuration
+    hub_room_config = {
+        # The pin the motion sensor is attached to.
+        "sensing_pin": 4
+        }
 
-# Represenst the hub room configuration
-hub_room_config = {
-    # The pin the motion sensor is attached to.
-    "sensing_pin": 7
-    }
+else: 
+    
+    # Occupied state configuration
+    occupied_config = {
+        # Time to wait for no detections thereby assuming the room is now unoccupied
+        # Seconds
+        "timeout": 10
+        }
+    
+    # Unoccupied state configuration
+    unoccupied_config = {
+        # The time between consecutive detections in seconds that contribute to the 
+        # 'threshold' argument. We look for <= the interval between detections.
+        "max_detection_interval": 5, 
+        
+        # The number of consecutive detections with the specified interval required 
+        # before we assume the room is occupied
+        "required_consecutive_detections": 1
+        }
+    
+    # Represenst the hub room configuration
+    hub_room_config = {
+        # The pin the motion sensor is attached to.
+        "sensing_pin": 7
+        }
